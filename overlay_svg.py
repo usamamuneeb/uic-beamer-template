f85c0df6531510cc01b39308d314c2321847ce51
Usama Muneeb <umunee2@uic.edu>
2024-07-21 15:32:38 -0500

    Add white departmental backgrounds

--- overlay_svg.py
+++ overlay_svg.py
@@ -13,13 +13,15 @@ from PIL import Image
 import cairosvg
 import io
 
-svg_path = "COL.ENG.ECE.LOCKC.SM.INVT.RGB.SVG"
+# svg_path = "COL.ENG.ECE.LOCKC.SM.RGB.SVG" # RGB Logo
+svg_path = "COL.ENG.ECE.LOCKC.SM.INVT.RGB.SVG" # Inverted RGB Logo
 svg_data = open(svg_path, 'rb').read()
 
 overlay_width = 2016
 left_offset = 4032//2 - overlay_width//2
 png_output = cairosvg.svg2png(svg_data, output_width=overlay_width)
 svg_img = Image.open(io.BytesIO(png_output))
+# background_color = (242, 247, 235)  # This color is Expo White
 background_color = (0, 30, 98)  # This color is Navy Pier Blue
 img = Image.new('RGB', (4032,3024), background_color)
 img.paste(svg_img, (left_offset, 2016), svg_img)
